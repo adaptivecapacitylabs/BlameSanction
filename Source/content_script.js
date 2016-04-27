a56228bbfb40f61a07ed1dbcf1226896b28786f7
John Allspaw <allspaw@gmail.com>
2016-04-27 18:44:33 +0200

    Let me fix that for you.

--- Source/content_script.js
+++ Source/content_script.js
@@ -36,10 +36,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bDevOps\b/g, "Cooperation, Collaboration, and Coordination");
+	v = v.replace(/\bDEVOPS\b/g, "Cooperation, Collaboration, and Coordination");
+	v = v.replace(/\bdevops\b/g, "cooperation, collaboration, and coordination");
 	
 	textNode.nodeValue = v;
 }
